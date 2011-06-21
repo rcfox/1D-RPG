2a5a773c07b5b757f731d229d7b970efa34b98f6
Ryan Fox <foxryan@gmail.com>
2011-06-20 21:27:13 -0400

    Used Emacs' folding-mode to help clean up the code. Also, moved all of the components to the top of the file, out of a specific scene.

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ window.onload = function() {
 	//start crafty
 	Crafty.init(88,31);
 
-	Crafty.extend({
+	Crafty.extend({ // delay_time, delay_frames
+		//{
 		__delay_entities: [],
 		delay_time: function(time,callback) {
 			this.__delay_entities.push(Crafty.e().attr({
@@ -41,6 +42,164 @@ window.onload = function() {
 			return this;
 		}
 	});
+	//}
+
+	Crafty.c("parallax", {
+		//{
+		_parallax_width: Crafty.viewport.width,
+		parallax: function(speed,width)
+		{
+			this.each(function() {
+				if(speed) this.speed = speed;
+				if(width) this._parallax_width = width;
+				this._extend_image();
+				this.start();
+			});
+			return this;
+		},
+		start: function(speed)
+		{
+			this.each(function() {
+				if(speed) this.speed *= speed;
+				this.bind("EnterFrame",function() {
+					this.x -= this.speed;
+					if(this.x <= -(this.w-this._parallax_width)) this.x = 0;
+					if(this.x > 0) this.x = -(this.w-this._parallax_width);
+				});
+			});
+			return this;
+		},
+		stop: function()
+		{
+			this.each(function() {
+				this.unbind("EnterFrame");
+			});
+			return this;
+		},
+		_extend_image: function() {
+			var canvas = document.createElement('canvas');
+			var canvasContext = canvas.getContext('2d');
+
+			var imgW = this.img.width;
+			var imgH = this.img.height;
+			canvas.width = imgW + this._parallax_width;
+			canvas.height = imgH;
+			canvasContext.drawImage(this.img, 0, 0);
+			canvasContext.putImageData(canvasContext.getImageData(0, 0, imgW, imgH),imgW,0);
+
+			this.image(canvas.toDataURL());
+		}
+	});
+	//}
+
+	Crafty.c("AutoHider", {
+		//{
+		_auto_hide: "AutoHide",
+		init: function() {
+			if(!this.has("Mouse")) this.addComponent("Mouse");
+			this.bind("MouseOver",function() {
+				Crafty("2D "+this._auto_hide).each(function() {
+					this.visible = true;
+				});
+			})
+				.bind("MouseOut",function() {
+					Crafty("2D "+this._auto_hide).each(function() {
+						this.visible = false;
+					});
+				});
+			return this;
+		},
+		autohide: function(type) {
+			this._auto_hide = type;
+		}
+	});
+	//}
+
+	Crafty.c("Icon", {
+		//{
+		_name: "",
+		selected: false,
+		icon: function(name,selected) {
+			this._name = name;
+			if(selected) this.selected = selected;
+			if(!this.has("Mouse")) this.addComponent("Mouse");
+			if(!this.has("Image")) this.addComponent("Image");
+			this.addComponent("Icon-"+name);
+
+			this.z = 2;
+			this.image(this._name+".png");
+			this.bind("MouseDown", function() {
+				Crafty("Icon").select(false);
+				this.select(true);
+			});
+			return this;
+		},
+		select: function(is_selected)
+		{
+			this.each(function() {
+				if(is_selected != undefined) this.selected = is_selected;
+
+				if(this.selected) {
+					this.image(this._name+"-glow.png");
+				}
+				else {
+					this.image(this._name+".png");
+				}
+			});
+			return this;
+		}
+	});
+	//}
+
+	Crafty.c("Follows", {
+		//{
+		_target: null,
+		_x_offset: undefined,
+		_y_offset: undefined,
+		follow: function(target, x_offset, y_offset) {
+			this.each(function () {
+				this._target = target;
+				if(target.has("AutoHider"))
+				{
+					if(!this.has("AutoHider")) this.addComponent("AutoHider");
+					this.autohide(target._auto_hide);
+				}
+				if(typeof x_offset !== 'undefined') this._x_offset = x_offset;
+				if(typeof y_offset !== 'undefined') this._y_offset = y_offset;
+				this.bind("EnterFrame",function() {
+					if(typeof this._x_offset !== 'undefined') this.x = this._target.x + this._x_offset;
+					if(typeof this._y_offset !== 'undefined') this.y = this._target.y + this._y_offset;
+				});
+			});
+			return this;
+		}
+	});
+	//}
+
+	Crafty.c("HealthBar", {
+		//{
+		_outline: null,
+		_max_height: 0,
+		_orig_y: 0,
+		health_bar: function(x,y,width, height, image) {
+			this.x = x;
+			this.y = y;
+			this.w = width;
+			this.h = height;
+			this.image(image,"repeat");
+			this._max_height = height;
+			this._orig_y = y;
+			return this;
+		},
+		update: function(ratio) {
+			this.each(function() {
+				this.h = this._max_height * ratio;
+				this.y = this._orig_y + this._max_height * (1-ratio);
+			});
+			return this;
+		}
+	});
+	//}
 
 	Crafty.sprite(28, "sprites.png", {
 		player: [0,0],
@@ -68,77 +227,12 @@ window.onload = function() {
 	Crafty.scene("loading");
 
 	Crafty.scene("main",function() {
-		Crafty.c("parallax", {
-			_parallax_width: Crafty.viewport.width,
-			parallax: function(speed,width)
-			{
-				this.each(function() {
-					if(speed) this.speed = speed;
-					if(width) this._parallax_width = width;
-					this._extend_image();
-					this.start();
-				});
-				return this;
-			},
-			start: function(speed)
-			{
-				this.each(function() {
-					if(speed) this.speed *= speed;
-					this.bind("EnterFrame",function() {
-						this.x -= this.speed;
-						if(this.x <= -(this.w-this._parallax_width)) this.x = 0;
-						if(this.x > 0) this.x = -(this.w-this._parallax_width);
-					});
-				});
-				return this;
-			},
-			stop: function()
-			{
-				this.each(function() {
-					this.unbind("EnterFrame");
-				});
-				return this;
-			},
-			_extend_image: function() {
-				var canvas = document.createElement('canvas');
-				var canvasContext = canvas.getContext('2d');
-
-				var imgW = this.img.width;
-				var imgH = this.img.height;
-				canvas.width = imgW + this._parallax_width;
-				canvas.height = imgH;
-				canvasContext.drawImage(this.img, 0, 0);
-				canvasContext.putImageData(canvasContext.getImageData(0, 0, imgW, imgH),imgW,0);
-
-				this.image(canvas.toDataURL());
-			}
-		});
 
 		var layers = [Crafty.e("2D, DOM, Image").image("layer0.png"),
 					  Crafty.e("2D, DOM, Image, parallax").image("layer1.png").parallax(0.2),
 					  Crafty.e("2D, DOM, Image, parallax").image("layer2.png").parallax(0.5),
 					  Crafty.e("2D, DOM, Image, parallax").image("layer3.png").parallax(0.7)];
 
-		Crafty.c("AutoHider", {
-			_auto_hide: "AutoHide",
-			init: function() {
-				if(!this.has("Mouse")) this.addComponent("Mouse");
-				this.bind("MouseOver",function() {
-					Crafty("2D "+this._auto_hide).each(function() {
-						this.visible = true;
-					});
-				})
-				.bind("MouseOut",function() {
-					Crafty("2D "+this._auto_hide).each(function() {
-						this.visible = false;
-					});
-				});
-				return this;
-			},
-			autohide: function(type) {
-				this._auto_hide = type;
-			}
-		});
 
 		var guy = Crafty.e("2D, DOM, player, SpriteAnimation, Tween, AutoHider")
 		  .attr({x:0,y:5,z:1})
@@ -146,63 +240,6 @@ window.onload = function() {
 		  .animate("walk_left",1,0,4)
 		  .animate("attack_right",0,1,2)
 		  .animate("walk_right",20,-1);
-
-		Crafty.c("Icon", {
-			_name: "",
-			selected: false,
-			icon: function(name,selected) {
-				this._name = name;
-				if(selected) this.selected = selected;
-				if(!this.has("Mouse")) this.addComponent("Mouse");
-				if(!this.has("Image")) this.addComponent("Image");
-				this.addComponent("Icon-"+name);
-
-				this.z = 2;
-				this.image(this._name+".png");
-				this.bind("MouseDown", function() {
-					Crafty("Icon").select(false);
-					this.select(true);
-				});
-				return this;
-			},
-			select: function(is_selected)
-			{
-				this.each(function() {
-					if(is_selected != undefined) this.selected = is_selected;
-
-					if(this.selected) {
-						this.image(this._name+"-glow.png");
-					}
-					else {
-						this.image(this._name+".png");
-					}
-				});
-				return this;
-			}
-		});
-
-		Crafty.c("Follows", {
-			_target: null,
-			_x_offset: undefined,
-			_y_offset: undefined,
-			follow: function(target, x_offset, y_offset) {
-				this.each(function () {
-					this._target = target;
-					if(target.has("AutoHider"))
-					{
-						if(!this.has("AutoHider")) this.addComponent("AutoHider");
-						this.autohide(target._auto_hide);
-					}
-					if(typeof x_offset !== 'undefined') this._x_offset = x_offset;
-					if(typeof y_offset !== 'undefined') this._y_offset = y_offset;
-					this.bind("EnterFrame",function() {
-						if(typeof this._x_offset !== 'undefined') this.x = this._target.x + this._x_offset;
-						if(typeof this._y_offset !== 'undefined') this.y = this._target.y + this._y_offset;
-					});
-				});
-				return this;
-			}
-		});
 
 		var sword_icon = Crafty.e("2D, DOM, Icon, Follows, AutoHide")
 		  .icon("sword")
@@ -217,28 +254,6 @@ window.onload = function() {
 		  .icon("flee")
 		  .follow(shield_icon,shield_icon.w,0);
 
-		Crafty.c("HealthBar", {
-			_outline: null,
-			_max_height: 0,
-			_orig_y: 0,
-			health_bar: function(x,y,width, height, image) {
-				this.x = x;
-				this.y = y;
-				this.w = width;
-				this.h = height;
-				this.image(image,"repeat");
-				this._max_height = height;
-				this._orig_y = y;
-				return this;
-			},
-			update: function(ratio) {
-				this.each(function() {
-					this.h = this._max_height * ratio;
-					this.y = this._orig_y + this._max_height * (1-ratio);
-				});
-				return this;
-			}
-		});
 
 		var health_bar = Crafty.e("2D, DOM, Image, HealthBar, Follows, AutoHide")
 		  .health_bar(guy.x+2,guy.y+9,3,15,"bar-filling.png")
